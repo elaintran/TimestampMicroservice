c5cb167b1d6cfd8c7dbfb5e6ec31ed2557a7c0dd
elaintran <elain.ttran@gmail.com>
2019-02-23 23:11:03 -0600

    get route

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
-var cors = require('cors');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
 var app = express();
+//app.use(bodyParser.json());
+//app.use(cors());
+
+//get api routes
+var api = require("./routes/api")
 
 
 //express server & serve HTML file
@@ -9,9 +15,15 @@ app.get("/", function(req, res) {
     res.sendFile( __dirname + "/views/index.html");
 });
 
+// css folder
+app.use(express.static('public'));
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  	res.json({greeting: 'hello API'});
+});
+
+
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
 })
-
-// css folder
-app.use(express.static('public'));
