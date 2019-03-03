f3d6d5dd4f43aa2cb2366ac0c762b6059b24bffc
elaintran <elain.ttran@gmail.com>
2019-03-02 23:25:36 -0600

    date string testing

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ var moment = require("moment");
 var app = express();
 
 //get route
-var route = require("./routes/routes.js");
+//var route = require("./routes/routes.js");
+
+app.get("/api/timestamp/:date_string?", function(req, res) {
+	var date_string = req.params.date_string;
+	console.log(date_string);
+	var date;
+	if (!date_string) {
+		date = new Date();
+	}
+	//res.send(date_string);
+	res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
 
 //route to routes
-app.use("/api/", route);
+/*app.use("/api/", route);
 app.use("/api/v1", route);
-app.use("/", route);
+app.use("/", route);*/
 
 //express server & serve HTML file
 app.get("/", function(req, res) {
